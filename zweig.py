9614baf707d86089b8f27faabc2a7f1f9465f652
Stefan Liu <stefan.liu@nsn.com>
2016-06-07 17:41:25 +0800

    simple ssh connection

--- zweig.py
+++ zweig.py
@@ -1,6 +1,21 @@
 from robot.libraries.BuiltIn import BuiltIn
 import os
+from tdd_kiss_core.sshconnection.sshconnection import SshConnection
 
 def tell_macro_value():
-	blt = BuiltIn()
-	return os.path.dirname(blt.get_variables()['${SUITE_SOURCE}'])
+    blt = BuiltIn()
+    return os.path.dirname(blt.get_variables()['${SUITE_SOURCE}'])
+
+def run_command_under_ssh_localhost(cmd):
+    execute_command_by_ssh(cmd)
+
+def execute_command_by_ssh(cmd):
+    ssh_client = SshConnection("127.0.0.1", "ute", "ute", 22, prompt="$ ")
+    try:
+        ssh_client._setup()
+        ret = ssh_client.execute(cmd)
+    except Exception, e:
+        pass
+    finally:
+        ssh_client._teardown()
+        return ret
